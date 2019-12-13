39fcbdb9f09f9c93dd991b29d2a1290d621b0a06
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-12-13 17:22:38 -0600

    Switch to comptime function since expressions must be comptime known

--- src/main.zig
+++ src/main.zig
@@ -1,31 +1,29 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const testing = std.testing;
 
-const Expression = enum {
-    placeholder,
+const Expression = struct {
+    name: []const u8,
+    typeId: builtin.TypeId,
+    format: void,
 };
 
-const Template = struct {
-    literals: [][]const u8,
-    expressions: []Expression,
+fn Template(comptime fmt: []const u8) type {
+    comptime var build_literals = [_][]const u8{undefined};
+    comptime var build_expressions = [_]Expression{};
 
-    pub fn init(comptime fmt: []const u8) Template {
-        comptime var build_literals = [_][]const u8{undefined};
-        comptime var build_expressions = [_]Expression{};
+    build_literals[0] = fmt;
 
-        build_literals[0] = fmt;
+    return struct {
+        const literals = build_literals;
+        const expressions = build_expressions;
 
-        return .{
-            .literals = &build_literals,
-            .expressions = &build_expressions,
-        };
-    }
-
-    fn runAlloc(allocator: *std.mem.Allocator, args: var) void {}
-};
+        fn runAlloc(allocator: *std.mem.Allocator, args: var) void {}
+    };
+}
 
 test "basic init" {
-    const tmpl = Template.init("hello");
+    const tmpl = Template("hello");
 
     testing.expectEqual(tmpl.literals.len, 1);
     testing.expectEqual(tmpl.expressions.len, 0);
