93f2bd67271d0cef0e89ab1d6fb36d2196668710
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-12-13 18:32:24 -0600

    Super rudimentary parsing

--- src/main.zig
+++ src/main.zig
@@ -5,14 +5,63 @@ const testing = std.testing;
 const Expression = struct {
     name: []const u8,
     typeId: builtin.TypeId,
-    format: void,
+    format: void = {},
 };
 
-fn Template(comptime fmt: []const u8) type {
-    comptime var build_literals = [_][]const u8{undefined};
-    comptime var build_expressions = [_]Expression{};
+fn countExprs(str: []const u8) usize {
+    var result: usize = 0;
+    for (str) |c| {
+        if (c == '{') {
+            result += 1;
+        }
+    }
+    return result;
+}
 
-    build_literals[0] = fmt;
+fn Template(comptime fmt: []const u8) type {
+    const num_exprs = countExprs(fmt);
+
+    comptime var build_literals: [num_exprs + 1][]const u8 = undefined;
+    comptime var build_expressions: [num_exprs]Expression = undefined;
+
+    const State = enum {
+        Literal,
+        Expression,
+    };
+
+    comptime var start_idx = 0;
+    comptime var build_idx = 0;
+
+    comptime var state: State = .Literal;
+
+    for (fmt) |c, i| {
+        switch (c) {
+            '{' => {
+                if (state == .Expression) {
+                    @compileError("Too many levels of '{' at " ++ i);
+                }
+                build_literals[build_idx] = fmt[start_idx..i];
+                state = .Expression;
+            },
+            '}' => {
+                if (state == .Literal) {
+                    @compileError("Unmatched '}' at " ++ i);
+                }
+                build_expressions[build_idx] = .{
+                    .name = fmt[start_idx .. i + 1],
+                    .typeId = .Int,
+                };
+                start_idx = i + 1;
+                build_idx += 1;
+                state = .Literal;
+            },
+            else => {},
+        }
+    }
+    if (state == .Expression) {
+        @compileError("Unmatched '{' at " ++ start_idx);
+    }
+    build_literals[build_idx] = fmt[start_idx..];
 
     return struct {
         const literals = build_literals;
@@ -23,9 +72,19 @@ fn Template(comptime fmt: []const u8) type {
 }
 
 test "basic init" {
-    const tmpl = Template("hello");
+    {
+        const tmpl = Template("hello");
 
-    testing.expectEqual(tmpl.literals.len, 1);
-    testing.expectEqual(tmpl.expressions.len, 0);
-    testing.expectEqual(tmpl.literals[0], "hello");
+        testing.expectEqual(tmpl.literals.len, 1);
+        testing.expectEqual(tmpl.expressions.len, 0);
+        testing.expectEqualSlices(u8, tmpl.literals[0], "hello");
+    }
+
+    {
+        const tmpl = Template("hello{0}world");
+        testing.expectEqual(tmpl.literals.len, 2);
+        testing.expectEqual(tmpl.expressions.len, 1);
+        testing.expectEqualSlices(u8, tmpl.literals[0], "hello");
+        testing.expectEqualSlices(u8, tmpl.literals[1], "world");
+    }
 }
